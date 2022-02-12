8f5aac5c7fe061734abd4a7a5e85748adfea8613
lwk <1076278852@qq.com>
2022-02-12 19:59:04 +0800

    test: verify cred liuwenkai@huawei.com
    Signed-off-by: lwk <1076278852@qq.com>

--- oem_property/ohos/impl/dslm_ohos_verify.c
+++ oem_property/ohos/impl/dslm_ohos_verify.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -588,9 +588,8 @@ int32_t VerifyOhosDslmCred(const DeviceIdentify *device, uint64_t challenge, con
     DslmCredInfo *credInfo)
 {
     SECURITY_LOG_INFO("Invoke VerifyOhosDslmCred");
-
     struct CertChainValidateResult resultInfo;
-    (void)memset_s(&resultInfo, sizeof(struct CertChainValidateResult), 0, sizeof(struct CertChainValidateResult));
+    InitCertChainValidateResult(&resultInfo, credBuff->credLen);
 
     int32_t ret = ERR_DEFAULT;
     do {
@@ -623,6 +622,6 @@ int32_t VerifyOhosDslmCred(const DeviceIdentify *device, uint64_t challenge, con
 
     } while (0);
 
-    FreeCertChainValidateResult(&resultInfo);
+    DestroyCertChainValidateResult(&resultInfo);
     return ret;
 }
