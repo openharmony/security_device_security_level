f9f6166cb23136b3accf85efb840496daeb7286a
Zen知仁 <xuzhiren@huawei.com>
2022-03-29 12:49:00 +0800

    feat: hidumper output
    Signed-off-by: Zen知仁 <xuzhiren@huawei.com>
    Change-Id: Icac7c465d23a0747c31703a5e986b8bc87c96b71

--- services/dslm/dslm_hidumper.c
+++ services/dslm/dslm_hidumper.c
@@ -13,20 +13,53 @@
  * limitations under the License.
  */
 
-#include "dslm_hidumper.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "dslm_device_list.h"
+#include "dslm_hidumper.h"
+
+#define SPLIT_LINE "------------------------------------------------------"
+#define END_LINE "\n"
+
 static void PrintBanner(int fd)
 {
-    dprintf(fd, "  ___  ___ _    __  __   ___  _   _ __  __ ___ ___ ___ \n");
-    dprintf(fd, " |   \\/ __| |  |  \\/  | |   \\| | | |  \\/  | _ \\ __| _ \\\n");
-    dprintf(fd, " | |) \\__ \\ |__| |\\/| | | |) | |_| | |\\/| |  _/ __|   /\n");
-    dprintf(fd, " |___/|___/____|_|  |_| |___/ \\___/|_|  |_|_| |___|_|_\\\n");
+    dprintf(fd, " ___  ___ _    __  __   ___  _   _ __  __ ___ ___ ___ " END_LINE);
+    dprintf(fd, "|   \\/ __| |  |  \\/  | |   \\| | | |  \\/  | _ \\ __| _ \\" END_LINE);
+    dprintf(fd, "| |) \\__ \\ |__| |\\/| | | |) | |_| | |\\/| |  _/ __|   /" END_LINE);
+    dprintf(fd, "|___/|___/____|_|  |_| |___/ \\___/|_|  |_|_| |___|_|_\\" END_LINE);
+}
+
+static void DumpOneDevice(const DslmDeviceInfo *info, int32_t fd)
+{
+    if (info == NULL) {
+        return;
+    }
+    dprintf(fd, SPLIT_LINE END_LINE);
+    dprintf(fd, "DEVICE_ID                 : %x" END_LINE, info->machine.machineId);
+    dprintf(fd, "DEVICE_TYPE               : %d" END_LINE, info->deviceType);
+    dprintf(fd, "DEVICE_ONLINE_STATUS      : %s" END_LINE, info->onlineStatus ? "online" : "offline");
+    dprintf(fd, "DEVICE_MACHINE_STATUS     : %d" END_LINE, info->machine.currState);
+    dprintf(fd, "DEVICE_VERIFIED_LEVEL     : %d" END_LINE, info->credInfo.credLevel);
+    dprintf(fd, "DEVICE_VERIFIED_RESULT    : %d" END_LINE, info->result);
+    dprintf(fd, "CRED_TYPE                 : %d" END_LINE, info->credInfo.credType);
+    dprintf(fd, "CRED_SIGNTIME             : %s" END_LINE, info->credInfo.signTime);
+    dprintf(fd, "CRED_MANUFACTURE          : %s" END_LINE, info->credInfo.manufacture);
+    dprintf(fd, "CRED_BAND                 : %s" END_LINE, info->credInfo.brand);
+    dprintf(fd, "CRED_MODEL                : %s" END_LINE, info->credInfo.model);
+    dprintf(fd, "CRED_SOFTWARE_VERSION     : %s" END_LINE, info->credInfo.softwareVersion);
+    dprintf(fd, "CRED_SECURITY_LEVEL       : %s" END_LINE, info->credInfo.securityLevel);
+    dprintf(fd, "CRED_VERSION              : %s" END_LINE, info->credInfo.version);
+    dprintf(fd, SPLIT_LINE END_LINE);
+}
+
+static void PrintAllDevices(int fd)
+{
+    ForEachDeviceDump(DumpOneDevice, fd);
 }
 
 void DslmDumper(int fd)
 {
     PrintBanner(fd);
+    PrintAllDevices(fd);
 }
