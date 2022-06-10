47799409696550db2b7c72d0d0ce6871c4a5b0ac
shipengtao9579 <shipengtao@huawei.com>
2022-06-10 11:48:45 +0800

    style: 修改hidumper 输出格式，更改文件行尾序列符号为LF
    Signed-off-by: shipengtao9579 <shipengtao@huawei.com>
    Change-Id: Id4c3e27ac6288c5a9eb98e49d7dddd86404fad1f

--- services/dslm/dslm_hidumper.c
+++ services/dslm/dslm_hidumper.c
@@ -191,7 +191,7 @@ static void PrintDefaultStatus(int fd)
     const time_t YEAR_TIME_2022 = 1640966400;
     struct timeval timeVal = {0};
     gettimeofday(&timeVal, NULL);
-    char *notice = timeVal.tv_sec <= YEAR_TIME_2022 ? ", Please check the system time." : "";
+    char *notice = timeVal.tv_sec <= YEAR_TIME_2022 ? "(please check the system time)" : "";
 
     dprintf(fd, SPLIT_LINE END_LINE);
     dprintf(fd, "REQUEST_TEST              : %s" END_LINE, requestResult == SUCCESS ? "success" : "failed");
