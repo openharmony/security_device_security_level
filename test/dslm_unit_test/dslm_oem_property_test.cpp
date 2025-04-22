a3a6f22e5c22f392d10a230dd7b442809f764380
zhuofan0129 <861080528@qq.com>
2025-04-22 15:37:45 +0800

    feat: sync 5.1.0
    Signed-off-by: zhuofan0129 <861080528@qq.com>

--- test/dslm_unit_test/dslm_oem_property_test.cpp
+++ test/dslm_unit_test/dslm_oem_property_test.cpp
@@ -86,9 +86,6 @@ HWTEST_F(DslmOemPropertyTest, VerifyDslmCredential_case1, TestSize.Level0)
 
     ret = VerifyDslmCredential(cred, &info, &list);
     EXPECT_EQ(ERR_PARSE_CLOUD_CRED_DATA, ret);
-
-    ret = VerifyDslmCredential(nullptr, &info, &list);
-    EXPECT_EQ(ERR_PARSE_CLOUD_CRED_DATA, ret);
 }
 
 /**
@@ -692,12 +689,6 @@ HWTEST_F(DslmOemPropertyTest, EcdsaVerify_case1, TestSize.Level0)
     uint32_t algorithm = TYPE_ECDSA_SHA_256;
 
     int32_t ret = EcdsaVerify(&srcData, &sigData, pbkData, algorithm);
-    EXPECT_EQ(ERR_INVALID_PARA, ret);
-
-    ret = EcdsaVerify(nullptr, &sigData, pbkData, algorithm);
-    EXPECT_EQ(ERR_INVALID_PARA, ret);
-
-    ret = EcdsaVerify(&srcData, nullptr, pbkData, algorithm);
     EXPECT_EQ(ERR_INVALID_PARA, ret);
 }
 
